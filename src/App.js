45dcb8dda548b4c5a50c9c2831ad684a4323b6a1
VladimirVolvakov <dr.volvakov@gmail.com>
2022-03-02 12:38:41 +0300

    feat: add explore page layout

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { ToastContainer } from 'react-toastify'
+import Category from './pages/Category'
 import Explore from './pages/Explore'
 import ForgotPassword from './pages/ForgotPassword'
 import Navbar from './components/Navbar'
@@ -18,6 +19,7 @@ function App() {
           <Route path='/' element={<Explore />} />
           <Route path='/forgot-password' element={<ForgotPassword />} />
           <Route path='/offers' element={<Offer />} />
+          <Route path='/category/:categoryName' element={<Category />} />
           <Route path='/profile' element={<PrivateRoute />}>
             <Route path='/profile' element={<Profile />} />
           </Route>
--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -1,7 +1,37 @@
+import { Link } from 'react-router-dom'
+import rentCategoryImage from '../assets/jpg/rentCategoryImage.jpg'
+import sellCategoryImage from '../assets/jpg/sellCategoryImage.jpg'
+
 const Explore = () => {
     return (
-        <div>
-            <h1>Explore</h1>
+        <div className='explore'>
+            <header>
+                <p className='pageHeader'>Explore</p>
+            </header>
+
+            <main>
+                {/* Slider */}
+
+                <p className='exploreCategoryHeading'>Categories</p>
+                <div className='exploreCategories'>
+                    <Link to='/category/rent'>
+                        <img 
+                            alt='Rent' 
+                            className='exploreCategoryImg' 
+                            src={ rentCategoryImage } 
+                        />
+                        <p className='exploreCategoryName'>Real estate for rent</p>
+                    </Link>
+                    <Link to='/category/sell'>
+                        <img 
+                            alt='Sell' 
+                            className='exploreCategoryImg' 
+                            src={ sellCategoryImage } 
+                        />
+                        <p className='exploreCategoryName'>Real estate for sale</p>
+                    </Link>
+                </div>
+            </main>
         </div>
     )
 }
