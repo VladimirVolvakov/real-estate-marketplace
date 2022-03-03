a1e6d7e86972d90dbf18508eacc6d2062fb54d53
VladimirVolvakov <dr.volvakov@gmail.com>
2022-03-03 22:21:31 +0300

    feat: add listing item layout

---
+++ src/components/ListingItem.jsx
@@ -0,0 +1,71 @@
+import { Link } from 'react-router-dom'
+import { ReactComponent as DeleteIcon } from '../assets/svg/deleteIcon.svg'
+import bathtubIcon from '../assets/svg/bathtubIcon.svg'
+import bedIcon from '../assets/svg/bedIcon.svg'
+
+const ListingItem = ({ listing, id, onDelete }) => {
+    return (
+        <li className='categoryListing'>
+            <Link 
+                className='categoryListingLink'
+                to={ `/category/${ listing.type }/${ id }` }
+            >
+                <img 
+                    alt={ listing.name } 
+                    className='categoryListingImg' 
+                    src={ listing.imageUrls[0] } 
+                />
+
+                <div className='categoryListingDetails'>
+                    <p className='categoryListingLocation'>
+                        { listing.location }
+                    </p>
+
+                    <p className='categoryListingName'>
+                        { listing.name }
+                    </p>
+
+                    <p className='categoryListingPrice'>
+                        ${ listing.offer 
+                            ? listing.discountedPrice 
+                                .toString()
+                                .replace(/\B(?=(\d{3})+(?!\d))/g, ',')
+                            : listing.regularPrice 
+                                .toString()
+                                .replace(/\B(?=(\d{3})+(?!\d))/g, ',')
+                        }
+                        { listing.type === 'rent' && ' / month'}
+                    </p>
+
+                    <div className='categoryListingInfoDiv'>
+                        <img alt='bed' src={ bedIcon } />
+                        <p className='categoryListingInfoText'>
+                            { listing.bedrooms > 1
+                                ? `${listing.bedrooms} bedrooms`
+                                : '1 bedroom'
+                            }
+                        </p>
+
+                        <img alt='bath' src={ bathtubIcon } />
+                        <p className='categoryListingInfoText'>
+                            { listing.bathrooms > 1
+                                ? `${listing.bathrooms} bathrooms`
+                                : '1 bathroom'
+                            }
+                        </p>
+                    </div>
+                </div>
+            </Link>
+
+            { onDelete && (
+                <DeleteIcon 
+                    className='removeIcon' 
+                    fill='rgb(231, 76, 60)' 
+                    onClick={ () => onDelete(listing.id, listing.name) }
+                />
+            ) }
+        </li>
+    )
+}
+
+export default ListingItem
--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -3,6 +3,7 @@ import { db } from '../firebase.config'
 import { toast } from 'react-toastify'
 import { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
+import ListingItem from '../components/ListingItem'
 import Spinner from '../components/Spinner'
 
 const Category = () => {
@@ -66,7 +67,11 @@ const Category = () => {
                             <main>
                                 <ul className='categoryListings'>
                                     { listings.map(listing => (
-                                        <h3 key={ listing.id }>{ listing.data.name }</h3>
+                                        <ListingItem 
+                                            id={ listing.id }
+                                            key={ listing.id }
+                                            listing={ listing.data } 
+                                        />
                                     )) }
                                 </ul>
                             </main>
