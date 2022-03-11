00847c5de33481f908e6d87dab5d338ac62bc7c4
VladimirVolvakov <dr.volvakov@gmail.com>
2022-03-11 18:46:34 +0300

    feat: add delete listings functionality

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,25 +1,58 @@
 import { db } from '../firebase.config'
+import { collection, deleteDoc, doc, getDocs, orderBy, query, updateDoc, where } from 'firebase/firestore'
 import { getAuth, updateProfile } from 'firebase/auth'
+import { Link, useNavigate } from 'react-router-dom'
 import { toast } from 'react-toastify'
-import { updateDoc,doc } from 'firebase/firestore'
 import { useEffect, useState } from 'react'
-import { useNavigate, Link } from 'react-router-dom'
 import arrowRight from '../assets/svg/keyboardArrowRightIcon.svg'
 import homeIcon from '../assets/svg/homeIcon.svg'
+import ListingItem from '../components/ListingItem'
 
 const Profile = () => {
     const auth = getAuth()
 
     const [changeDetails, setChangeDetails] = useState(false)
-
     const [formData, setFormData] = useState({
         name: auth.currentUser.displayName,
         email: auth.currentUser.email
     })
+    const [isLoading, setIsLoading] = useState(true)
+    const [listings, setListings] = useState(null)
 
     const {name, email} = formData
 
     const navigate = useNavigate()
+
+    useEffect(() => {
+        const fetchUserListings = async () => {
+            // Create a reference:
+            const listingsRef = collection(db, 'listings')
+
+            // Create a query (we get listings in descending timestamp order when userRef is equal to logged in user):
+            const q = query(listingsRef, where('userRef', '==', auth.currentUser.uid), orderBy('timestamp', 'desc'))
+
+            // Get a query snapshot:
+            const querySnap = await getDocs(q)
+
+            // Initialize a value:
+            let listings = []
+
+            // Fill in the listings array with found docs:
+            querySnap.forEach(doc => {
+                return listings.push({
+                    id: doc.id,
+                    data: doc.data()
+                })
+            })
+
+            // Set new value of listings array:
+            setListings(listings)
+
+            setIsLoading(false)
+        }
+
+        fetchUserListings()
+    }, [auth.currentUser.uid])
 
     const onChange = e => {
         setFormData(prevState => ({
@@ -49,6 +82,19 @@ const Profile = () => {
             }
         } catch (error) {
             toast.error('Could not update profile details. Something went wrong...')
+        }
+    }
+
+    const onDelete = async (listingId) => {
+        if (window.confirm('Are you sure you want to delete this listing?')) {
+            // Delete document with id = listingId:
+            await deleteDoc(doc(db, 'listings', listingId))
+            // Update initial listings:
+            const updatedListings = listings.filter(listing => listing.id !== listingId)
+            // Set listings equal to updated listings:
+            setListings(updatedListings)
+            // Tell everything went correct:
+            toast.success('Listing deleted successfully')
         }
     }
 
@@ -107,6 +153,22 @@ const Profile = () => {
                         src={arrowRight} 
                     />
                 </Link>
+
+                { !isLoading && listings?.length > 0 && (
+                    <>
+                        <p className='listingText'>Your listings</p>
+                        <ul className='listingsList'>
+                            { listings.map(listing => (
+                                <ListingItem 
+                                    id={listing.id}
+                                    key={listing.id}
+                                    listing={listing.data}
+                                    onDelete={() => onDelete(listing.id)}
+                                />
+                            )) }
+                        </ul>
+                    </>
+                ) }
             </main>
         </div>
     )
